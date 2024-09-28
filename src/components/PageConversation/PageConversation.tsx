0300c97ba51407537b30e015d28d8d7fe30ed3d0
Darlley <systembrtt@gmail.com>
2024-09-28 13:43:53 -0400

    fix: loading e error

--- src/components/PageConversation/PageConversation.tsx
+++ src/components/PageConversation/PageConversation.tsx
@@ -22,6 +22,7 @@ import {
   Bird,
   Bot,
   CornerDownLeft,
+  LoaderCircle,
   Mic,
   Paperclip,
   Rabbit,
@@ -34,7 +35,6 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -47,9 +47,12 @@ export default function PageConversation(props: PageConversationProps) {
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      prompt: ''
+      prompt: '',
     },
   });
+
+  const { isLoading, isSubmitSuccessful, isSubmitting, errors } =
+    form.formState;
 
   /**
    * Função chamada ao submeter o formulário.
@@ -67,7 +70,13 @@ export default function PageConversation(props: PageConversationProps) {
    * das funcionalidades do react-hook-form, como a manipulação de erros e o estado de submissão.
    */
   const onSubmit: SubmitHandler<FormSchema> = async (data) => {
-    console.log("data", data);
+    return new Promise<void>((resolve) => {
+      // Removido o tipo Promise<void>
+      setTimeout(() => {
+        console.log('data', data);
+        resolve(); // resolve() não precisa de tipo
+      }, 5000);
+    });
   };
 
   return (
@@ -188,7 +197,11 @@ export default function PageConversation(props: PageConversationProps) {
       </div>
       <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
         <Badge className="absolute right-3 top-3">Output</Badge>
-        <div className="flex-1" />
+        <div className='flex grow'>
+
+          message content
+
+        </div>
         <Form {...form}>
           <form
             className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
@@ -204,41 +217,58 @@ export default function PageConversation(props: PageConversationProps) {
                   <FormControl>
                     <Textarea
                       {...field}
+                      disabled={isSubmitting}
                       placeholder="Escreva sua mensagem."
                       className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
                     />
                   </FormControl>
-                  <FormMessage className='p-3' />
                 </FormItem>
               )}
             />
-            <div className="flex items-center p-3 pt-0">
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Button variant="ghost" size="icon">
-                      <Paperclip className="size-4" />
-                      <span className="sr-only">Attach file</span>
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent side="top">Attach File</TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Button variant="ghost" size="icon">
-                      <Mic className="size-4" />
-                      <span className="sr-only">Use Microphone</span>
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent side="top">Use Microphone</TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-              <Button type="submit" size="sm" className="ml-auto gap-1.5">
-                Send Message
-                <CornerDownLeft className="size-3.5" />
-              </Button>
+            <div className="flex items-center justify-between p-3 pt-0">
+              <div className="flex items-center">
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="ghost" size="icon">
+                        <Paperclip className="size-4" />
+                        <span className="sr-only">Attach file</span>
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">Attach File</TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="ghost" size="icon">
+                        <Mic className="size-4" />
+                        <span className="sr-only">Use Microphone</span>
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">Use Microphone</TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </div>
+              <div className='flex items-center gap-2'>
+                {!!errors.prompt && (
+                  <p className='text-destructive text-xs'>{errors?.prompt?.message}</p>
+                )}
+                <Button
+                  type="submit"
+                  size="sm"
+                  variant={!!errors.prompt ? 'destructive' : 'default'}
+                  className="ml-auto gap-1.5"
+                  disabled={isSubmitting}
+                >
+                  Send Message
+                  {isSubmitting ? (
+                    <LoaderCircle className="size-3.5 animate-spin" />
+                  ) : (
+                    <CornerDownLeft className="size-3.5" />
+                  )}
+                </Button>
+              </div>
             </div>
           </form>
         </Form>
